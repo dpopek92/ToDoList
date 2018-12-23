6f1f0774b57a6d187eb400eb9851f7ed1b658430
Damian <d.popek92@gmail.com>
2018-12-23 15:34:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,70 +1,76 @@
- //zmienne globalne!
- let taskButton = null;
- let taskSearch = null;
- let taskBoard = null;
+  let taskButton = null;
+  let taskSearch = null;
+  let taskBoard = null;
 
- const addTask = function (text) {
-   const task = document.createElement('div');
-   task.classList.add('todo-task')
+  const addTask = function (text) {
+    const task = document.createElement('div');
+    task.classList.add('todo-task')
 
-   const taskBar = document.createElement('div');
-   taskBar.classList.add('todo-task-bar');
+    const taskBar = document.createElement('div');
+    taskBar.classList.add('todo-task-bar');
 
-   const taskData = new Date();
+    const taskData = new Date();
 
-   const taskRemoveBtn = document.createElement('button');
-   taskRemoveBtn.classList.add('button', 'todo-task-remove');
-   taskRemoveBtn.textContent = 'X';
+    const taskRemoveBtn = document.createElement('button');
+    taskRemoveBtn.classList.add('button', 'todo-task-remove');
+    taskRemoveBtn.textContent = 'X';
 
-   const taskText = document.createElement('div');
-   taskText.classList.add('todo-task-text');
+    const taskText = document.createElement('div');
+    taskText.classList.add('todo-task-text');
 
-   taskBar.textContent = `${taskData.getDate()}-${taskData.getMonth()}-${taskData.getFullYear()}r. ${taskData.getHours()}:${taskData.getMinutes()}`;
-   taskBar.appendChild(taskRemoveBtn);
+    //RemoveButton
+    taskBar.appendChild(taskRemoveBtn);
+    //Data
+    taskBar.textContent = `${addZero(taskData.getDate())}-${addZero(taskData.getMonth())}-${addZero(taskData.getFullYear())}r. ${addZero(taskData.getHours())}:${addZero(taskData.getMinutes())}`;
+    //Zawartość
+    taskText.textContent = text;
 
-   taskText.textContent = text;
+    task.appendChild(taskBar);
+    task.appendChild(taskText);
 
-   task.appendChild(taskBar);
-   task.appendChild(taskText);
+    taskBoard.append(task);
+  }
 
-   taskBoard.append(task);
- }
+  const addZero = function (date) {
+    if (date < 10) {
+      return '0' + date
+    } else {
+      return date
+    }
+  }
 
- const searchTask = function () {
-   const tasks = [...taskBoard.querySelectorAll('.todo-task-text')];
-   tasks.forEach(function (el) {
-     if (el.textContent.includes(taskSearch.value)) {
-       el.parentElement.style.setProperty('display', '');
-     } else {
-       el.parentElement.style.setProperty('display', 'none');
-     }
-   })
- }
+  const searchTask = function () {
+    const tasks = [...taskBoard.querySelectorAll('.todo-task-text')];
+    tasks.forEach(function (el) {
+      if (el.textContent.includes(taskSearch.value)) {
+        el.parentElement.style.setProperty('display', '');
+      } else {
+        el.parentElement.style.setProperty('display', 'none');
+      }
+    })
+  }
 
+  document.addEventListener('DOMContentLoaded', function () {
+    taskButton = document.querySelector('#todoButton');
+    taskSearch = document.querySelector('#todoSearch');
+    taskBoard = document.querySelector('#todoRight');
 
- document.addEventListener('DOMContentLoaded', function () {
-   //przypisanie do zmiennych globalnych dopiero po wczytaniu drzewa DOM
-   taskButton = document.querySelector('#todoButton');
-   taskSearch = document.querySelector('#todoSearch');
-   taskBoard = document.querySelector('#todoRight');
+    taskButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      const taskMessage = document.querySelector('#todoMessage');
+      if (taskMessage.value) {
+        addTask(taskMessage.value);
+        taskMessage.value = '';
+      } else {
+        return;
+      }
+    });
 
-   taskButton.addEventListener('click', function (e) {
-     e.preventDefault();
-     const taskMessage = document.querySelector('#todoMessage');
-     if (taskMessage.value) {
-       addTask(taskMessage.value);
-       taskMessage.value = '';
-     } else {
-       return;
-     }
-   });
+    taskSearch.addEventListener('input', searchTask);
 
-   taskSearch.addEventListener('input', searchTask);
-   //e.target wskazuje na referencję do której zastosowano event
-   taskBoard.addEventListener('click', function (e) {
-     //closest wskazuje na najbliższy obiekt pasujący do selektora
-     if (e.target.closest('.todo-task-remove')) {
-       e.target.closest('.todo-task').remove();
-     }
-   })
- });
+    taskBoard.addEventListener('click', function (e) {
+      if (e.target.closest('.todo-task-remove')) {
+        e.target.closest('.todo-task').remove();
+      }
+    })
+  });
